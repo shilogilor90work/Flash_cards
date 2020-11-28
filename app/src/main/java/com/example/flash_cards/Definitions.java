1763d8c1ba0a97a485618332ee1caf08abdfbfcc
snir <snirsha1992@gmail.com>
2020-11-28 16:53:14 +0200

    NEW Definitions page - just starting....

---
+++ app/src/main/java/com/example/flash_cards/Definitions.java
@@ -0,0 +1,16 @@
+package com.example.flash_cards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Definitions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_definitions);
+        setTitle("Definitions");
+
+    }
+}
--- app/src/main/java/com/example/flash_cards/Subjects.java
+++ app/src/main/java/com/example/flash_cards/Subjects.java
@@ -2,14 +2,32 @@ package com.example.flash_cards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Subjects extends AppCompatActivity {
+
+    Button definitions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
+        setTitle("Subjects");
+
+        definitions = findViewById(R.id.Definitions2);
+
+        definitions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Subjects.this,Definitions.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
