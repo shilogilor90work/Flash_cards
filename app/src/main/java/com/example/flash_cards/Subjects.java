fc38cfed7ccc28e35520c3bcefc03ad42a74f9da
snir <snirsha1992@gmail.com>
2020-12-07 19:21:42 +0200

    one problem

--- app/src/main/java/com/example/flash_cards/Subjects.java
+++ app/src/main/java/com/example/flash_cards/Subjects.java
@@ -1,83 +1,92 @@
 package com.example.flash_cards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Subjects extends AppCompatActivity {
-    ListView subject_list;
-    ArrayList<String>subjects;
-    ArrayAdapter<String> adapter;
-    ArrayList<String> Subjectlist;
-    EditText search_words;
-    Button btn_search;
 
-    Button definitions;
-    Button friends;
+    Button definition_btn;
+    Button add_btn;
+    EditText subjects_txt;
+    ListView subject_list;
+
+    FirebaseAuth firebaseAuth;
+    DatabaseReference root_database;
+    ArrayList<String> subjectArrayList;
+    FirebaseUser user;
+    SubjectsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subjects);
-        setTitle("Subjects");
-        subject_list=(ListView)findViewById(R.id.subject_list);
-        subjects = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(Subjects.this, android.R.layout.simple_list_item_1, subjects);
-        subject_list.setAdapter(adapter);
-        search_words = (EditText) findViewById(R.id.subjects_search);
-        btn_search = (Button) findViewById(R.id.subjects_btn);
-        click_button();
+        setContentView(R.layout.activity_definitions);
+        setTitle("Definitions");
+        add_btn = (Button) findViewById(R.id.subjects_btn);
+        definition_btn = findViewById(R.id.Definitions2);
 
-        definitions = findViewById(R.id.Definitions2);
+        subjects_txt = (EditText) findViewById(R.id.subjects);
+        subject_list = (ListView) findViewById(R.id.subject_list);
 
-        definitions.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        subjectArrayList = new ArrayList<String>();
+        root_database = FirebaseDatabase.getInstance().getReference().child("users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                root_database.child(user.getEmail().substring(0, user.getEmail().indexOf("@"))).child("subjects").child("math").child(subjects_txt.getText().toString());
+                Toast.makeText(Subjects.this, "added data", Toast.LENGTH_SHORT).show();
+                subjectArrayList.add(subjects_txt.getText().toString());
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+        root_database.child(user.getEmail().substring(0, user.getEmail().indexOf("@"))).child("subjects").child("math").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                            subjectArrayList.add(dsp.getKey());
+                        }
+                        subject_list.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+        adapter = new SubjectsAdapter(this, subjectArrayList);
+
+
+
+        definition_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Subjects.this,Definitions.class);
                 startActivity(i);
             }
         });
-        friends = findViewById(R.id.go_to_friends);
-
-        friends.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Subjects.this,Friends.class);
-                startActivity(i);
-            }
-        });
-
-
     }
-
-    {
-
-    }
-    public void click_button() {
-        btn_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                subjects.add(search_words.getText().toString());
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-
 }
---
+++ app/src/main/java/com/example/flash_cards/SubjectsAdapter.java
@@ -0,0 +1,70 @@
+package com.example.flash_cards;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SubjectsAdapter extends ArrayAdapter {
+    DatabaseReference root_database;
+    FirebaseUser user;
+    ArrayList<String> records;
+    public SubjectsAdapter(Context context, ArrayList<String> records) {
+        super(context, 0, records);
+        this.records = records;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        root_database = FirebaseDatabase.getInstance().getReference().child("users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String item = (String) getItem(position);
+        if (convertView==null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.subjects_listview, null);
+        }
+        TextView subjects_text = (TextView) convertView.findViewById(R.id.subjects_text);
+        Button subject_delete = (Button) convertView.findViewById(R.id.subject_delete);
+        Button btn_definitions = (Button) convertView.findViewById(R.id.btn_definitions);
+
+        subjects_text.setText(item);
+
+        subject_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference db_node = root_database.child( user.getEmail().substring(0, user.getEmail().indexOf("@"))).child("subjects").child(item);
+                db_node.removeValue();
+                records.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        btn_definitions.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),Definitions.class);
+                i.putExtra("subject",item);
+                getContext().startActivity(i);
+            }
+        });
+
+        return convertView;
+    }
+}
